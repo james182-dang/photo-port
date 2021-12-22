19df712e51c972316d791c588366f9591c614411
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-22 17:34:58 -0600

    add contactForm, tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -9,9 +9,10 @@ const categories = [
         description: 'Portraits of people in my life'
     }
 ]
-
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -22,6 +23,8 @@ describe('Nav component', () => {
             categories = {categories}
             setCurrentCategory = {mockSetCurrentCategory}
             currentCategory = {mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
         />);
     });
 
